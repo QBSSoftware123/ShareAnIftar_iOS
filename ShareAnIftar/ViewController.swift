c183b7e255074e12d82abb607163eb38410b9560
Tauqeer Ahmed Khan <TauqeerAhmedKhan@tauqeers-macbook-pro.local>
2017-04-02 19:00:24 +0530

    Initial Commit

---
+++ ShareAnIftar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareAnIftar
+//
+//  Created by Tauqeer Ahmed Khan on 02/04/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
