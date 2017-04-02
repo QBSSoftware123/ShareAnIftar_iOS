37e43b37cfb698137d5ac6f9d7df6e87bb3bb50e
Tauqeer Ahmed Khan <TauqeerAhmedKhan@tauqeers-macbook-pro.local>
2017-04-02 21:10:57 +0530

    Initial commit for Base View Controller & Service Manager.

---
+++ ShareAnIftar/Modules/Base/SAIViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SAIViewController.swift
+//  ShareAnIftar
+//
+//  Created by Tauqeer Ahmed Khan on 02/04/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import UIKit
+
+class SAIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ShareAnIftar/Service Manager/SAIServiceManager.swift
@@ -0,0 +1,13 @@
+//
+//  SAIServiceManager.swift
+//  ShareAnIftar
+//
+//  Created by Tauqeer Ahmed Khan on 02/04/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import UIKit
+
+class SAIServiceManager: NSObject {
+
+}
