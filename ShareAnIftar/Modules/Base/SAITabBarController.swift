8b646498a6b54c9114fa0078d4adbfeda1b23b7c
tauqeer ahmed khan <you@example.com>
2017-04-05 17:41:31 +0530

    Code Changes for TabBarController.

---
+++ ShareAnIftar/Modules/Base/SAITabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  SAITabBarController.swift
+//  ShareAnIftar
+//
+//  Created by tauqeer ahmed khan on 05/04/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import UIKit
+
+class SAITabBarController: UITabBarController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ ShareAnIftar/Modules/Landing Page/Eid/SAIEidViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SAIEidViewController.swift
+//  ShareAnIftar
+//
+//  Created by tauqeer ahmed khan on 05/04/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import UIKit
+
+class SAIEidViewController: SAIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
