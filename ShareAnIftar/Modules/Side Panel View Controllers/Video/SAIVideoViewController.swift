7b22085d495a81e050030c72084b88bad7d0b760
tauqeer ahmed khan <you@example.com>
2017-04-13 17:50:31 +0530

    Code Changes for Video Library.

--- ShareAnIftar/Modules/Side Panel View Controllers/Video/SAIVideoViewController.swift
+++ ShareAnIftar/Modules/Side Panel View Controllers/Video/SAIVideoViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class SAIVideoViewController: SAIViewController {
+class SAIVideoViewController: SAIViewController , UITableViewDataSource {
 
+    // WebView Outlet to play YouTube Videos
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        addBlueGradientColor(GradientColor:SAIGradientColorConstants.SAIBlueGradient)
+        navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
     }
     
+    // MARK: -  TableView Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+      return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "videoCell", for: indexPath)
+    // getVideo(videoCode: "C0Z6tJdeQ_E?rel=0")
+      return cell
+    }
+    
+//    func getVideo(videoCode:String)
+//    {
+//        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+//        youTubeWebView.loadRequest(URLRequest(url: url!))
+//    }
+
+    @IBAction func DismissView(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
 }
