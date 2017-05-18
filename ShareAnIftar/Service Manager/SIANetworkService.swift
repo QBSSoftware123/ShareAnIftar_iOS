ac50cb53639a86d97759312b9352df6b668a3ac4
enthuCoder <danish2k6@gmail.com>
2017-05-19 00:31:20 +0530

    Adding the Network Layer for making service calls Added TODO comments for further changes needed. Added comments

--- ShareAnIftar/Service Manager/SIANetworkService.swift
+++ ShareAnIftar/Service Manager/SIANetworkService.swift
@@ -2,6 +2,9 @@
 //  SIANetworkService.swift
 //  ShareAnIftar
 //
+//  @Abstract:
+//         Main Transaction class to fire the request to the server. Add JSON parsing code
+//
 //  Created by Dilgir Siddiqui on 4/9/17.
 //  Copyright © 2017 QBS. All rights reserved.
 //
@@ -11,34 +14,36 @@ import Foundation
 class SIANetworkService: NSObject {
     private var task: URLSessionDataTask?
     
-    // TODO: Check the range of success and failure codes
-    fileprivate var successCodes: Range<Int> = 200..<299
-    fileprivate var failureCodes: Range<Int> = 400..<499
-    
-    func request(url: URL,
-                 method: HTTPMethod,
-                 params: [String: AnyObject]? = nil,
-                 headers: [String: String]? = nil,
-                 success: ((Data?) -> Void)? = nil,
-                 failure: ((_ data: (Data?), _ error: Error?, _ responseCode: Int) -> Void)? = nil) {
-        
-        let mutableRequest = NSMutableURLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0)
-        mutableRequest.allHTTPHeaderFields = headers
-        mutableRequest.httpMethod = method.rawValue
-        if let params = params {
-            mutableRequest.httpBody = try! JSONSerialization.data(withJSONObject: params, options: [])
-        }
-        
+    //-------------------------------------------------------------------------
+    // Use the below function to make network calls and parse the JSON response
+    //-------------------------------------------------------------------------
+    func sentRequestFor(serviceName: String, withParameters: [String: AnyObject]?) {
+        let sharedTransactionManager = SIATransactionManager.sharedTransactionManager
+        let url = sharedTransactionManager.serviceURLFor(service: serviceName)
+        // TODO: Replace replacementCode ("#@!$%") with actual service parameters
+
+        let serviceURL = URL.init(string: url)
         let session = URLSession.shared
-        // open func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Swift.Void) -> URLSessionDataTask
-        
-       // task = session.dataTask(with: mutableRequest) {(data, response, error) -> Void in
-            // Check if the response is a success or a failure and send appropriate callback
-       // }
-        
+        task = session.dataTask(with: serviceURL!, completionHandler: { (data, response, error) -> Void in
+            
+            // TODO: You can also return completionHandler from this method from calling function and do JSON parsing there to make it more cleaner. Some error handling needs to be done there
+            if response != nil {
+                do {
+                    let responseData = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                    print("Response: \(responseData)")
+                    // Parse the JSON after this into your model
+
+                } catch {
+                    // Do exception handling here
+                }
+            }
+        })
         task?.resume()
     }
     
+    //---------------------------------------------
+    // Cancel the in progress transaction if needed
+    //---------------------------------------------
     func cancel() {
         task?.cancel()
     }
---
+++ ShareAnIftar/Service Manager/SIATransactionManager.swift
@@ -0,0 +1,79 @@
+//
+//  SIATransactionManager.swift
+//  ShareAnIftar
+//
+//  @Abstract:
+//      Forms the request URL using serviceURLForService: function with placeholder string (#@!$%) for request parameters
+//
+//  Created by Dilgir Siddiqui on 5/18/17.
+//  Copyright © 2017 QBS. All rights reserved.
+//
+
+import Foundation
+
+class SIATransactionManager: NSObject {
+    
+    static let sharedTransactionManager = SIATransactionManager()
+    
+    var services = [String: AnyObject]()
+    let baseURL = "http://shareaniftar.com/api/"
+    let replacementCode = "#@!$%"
+    
+    //----------------------------------------------
+    // Initialize and fetch Services details from plist
+    //----------------------------------------------
+    override init() {
+        if let resource = Bundle.main.path(forResource: "Services", ofType: "plist"), let services = NSDictionary(contentsOfFile: resource) as? [String: AnyObject] {
+            self.services = services
+        }
+    }
+    
+    //----------------------------------------------
+    // Construct service URL with parameters
+    //----------------------------------------------
+    func serviceURLFor(service: String) -> String {
+        var serviceURL: String?
+        
+        switch service {
+        case "validate_otp", "generate_otp", "user_profile":
+            serviceURL = constructURLFor(service: service)
+        case "smile_counter":
+            serviceURL = "\(baseURL)\(service).php"
+        default:
+            // Error handling
+            break
+        }
+        
+        return serviceURL!
+    }
+    
+    //----------------------------------------------
+    // Helper function to get service parameters
+    //----------------------------------------------
+    func fetchServiceParametersFor(service: String) -> [String: AnyObject]? {
+        
+        if let parameters = self.services[service] {
+            return parameters as? [String : AnyObject]
+        } else {
+            return nil
+        }
+    }
+    
+    //---------------------------------------------------
+    // Helper function to construct URL for service calls
+    //---------------------------------------------------
+    func constructURLFor(service: String) -> String? {
+        guard let parameters = fetchServiceParametersFor(service: service) else {
+            // Error handling for service parameters not existing
+            return nil
+        }
+        var serviceURL = "\(baseURL)\(service).php?"
+        for (index, key) in parameters.keys.enumerated() {
+            serviceURL = "\(serviceURL)\(key)=\(replacementCode)"
+            if (index+1) != parameters.keys.count {
+                serviceURL = "\(serviceURL)&"
+            }
+        }
+        return serviceURL
+    }
+}
